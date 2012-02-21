735c6afddaa25144fbca8dcba7fa6676a37810b0
shekarp@pcweb.com <SPasumarthi@HSX00239.GNET.global.vpn>
2012-02-20 16:17:30 -0800

    Initia commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b20329d3100b2e1153e2541c02428caa39d3a3c6edf7e69a9faba56dc89a92c7cec8996e76ffa283710ba61c53b8f3391796829308d78c53e27e1208514c6e55'
